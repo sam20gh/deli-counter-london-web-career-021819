241923bfe4388e85cbf27723deceae32eaa025e9
sam ghoreishi <sam20gh@gmail.com>
2019-02-10 16:37:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 katz_deli = []
 def line(katz_deli)
   line_array = []
-	if katz_deli.empty?
-		puts "The line is currently empty."
-	else
+	if katz_deli.length == 0 
+    puts "The line is currently empty."
+  else
 		puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
 	end
 end
